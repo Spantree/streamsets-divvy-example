7a584295eda48a2f0c6f3c5526574f1c6c58e7ff
Ubuntu <ubuntu@ip-172-30-0-25.ec2.internal>
2017-02-08 16:06:05 +0000

    skipping 2013 divvy trips (as it has a different date format)

--- load-trips.sh
+++ load-trips.sh
@@ -9,7 +9,6 @@ DESTINATION="${__dir}/data/input/divvy/trips"
 mkdir -p $TMP_DIR $DESTINATION
 
 FILES=("\
-  Divvy_Stations_Trips_2013.zip \
   Divvy_Stations_Trips_2014_Q1Q2.zip \
   Divvy_Stations_Trips_2014_Q3Q4.zip \
   Divvy_Trips_2015-Q1Q2.zip \
@@ -18,13 +17,21 @@ FILES=("\
   Divvy_Trips_2016_Q3Q4.zip \
 ")
 
+cd $TMP_DIR
+
 for f in $FILES; do
   S3_URL="${S3_URL_PREFIX}/$f"
   if [ ! -f "${TMP_DIR}/$f" ]; then
     aws s3 cp $S3_URL $TMP_DIR
   fi
-  cd $DESTINATION
-  unzip -o "$TMP_DIR/*.zip" *.csv
+  unzip -o "*.zip"
 done
+
+for d in */; do
+  mv "$d/*Trips*.csv" .
+  rm -Rf $d
+done
+
+rm -Rf *Stations*.csv
 
 cd $__dir
